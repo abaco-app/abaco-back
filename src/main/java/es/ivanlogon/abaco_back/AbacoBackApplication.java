04a6c876713759400a3188079729cf61d074a1ba
Iván <ilgl2@alu.ua.es>
2025-06-16 12:13:34 +0200

    first commit

---
+++ src/main/java/es/ivanlogon/abaco_back/AbacoBackApplication.java
@@ -0,0 +1,13 @@
+package es.ivanlogon.abaco_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ivanlogon/abaco_back/AbacoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ivanlogon.abaco_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
